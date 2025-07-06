75393253b360bd3a4605b8fca42aafcfcca09de5
C0unterPo1nt <lucinamfarrell@gmail.com>
2025-07-06 14:43:04 -0700

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
