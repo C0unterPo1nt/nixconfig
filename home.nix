51d00e72fbff3f53532f6615ac5ac7e607078c5a
C0unterPo1nt <lucinamfarrell@gmail.com>
2025-09-03 01:00:31 -0700

    cleanup

--- home.nix
+++ home.nix
@@ -11,7 +11,6 @@
 
     home.pointerCursor = {
         gtk.enable = true;
-        # x11.enable = true;
         package = pkgs.bibata-cursors;
         name = "Bibata-Modern-Classic";
         size = 16;
