eedaefe9074ca1709e3cdfcb07cfcb58564da3d6
C0unterPo1nt <lucinamfarrell@gmail.com>
2025-09-14 00:14:47 -0700

    restyling

--- configs/dunst.nix
+++ configs/dunst.nix
@@ -6,12 +6,20 @@
         settings = with config.lib.stylix.colors; {
             global = {
                 monitor = 1;
-                width = "(300,300)";
+                width = "(250,250)";
                 height = "(50,100)";
                 origin = "bottom-center";
                 corner_radius = 10;
                 gap_size = 10;
+                padding = 4;
+                horizontal_padding = 4;
+                text_icon_padding = 8;
                 frame_width = 1;
+                icon_corner_radius = 10;
+                idle_threshold = 180;
+                line_height = 5;
+                min_icon_size = 64;
+                max_icon_size = 64;
             };
             urgency_low = {
                 background = lib.mkForce ("#" + base01-hex-r + base01-hex-g + base01-hex-b + "99");
