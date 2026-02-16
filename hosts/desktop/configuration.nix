3b1030cbdaf53d36a78063682d66e6390434c7ac
C0unterPo1nt <lucinamfarrell@gmail.com>
2026-02-16 14:05:10 -0800

    cleanup

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -5,28 +5,28 @@
       ../../configs/sunshine.nix
     ];
 
-  # Bootloader.
   boot.loader = {
     systemd-boot.enable = true;
     systemd-boot.configurationLimit = 6;
     efi.canTouchEfiVariables = true;
   };
 
-  networking.hostName = "nixos";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
+    hostName = "nixos";
+    nameservers = [ "10.0.0.21" ];
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    #firewall.allowedTCPPorts = [ ... ];
+    firewall.allowedUDPPorts = [ 5353 31382 ];
+  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  networking.networkmanager.dns = "none";
-  networking.nameservers = [ "10.0.0.21" ];
-  networking.dhcpcd.extraConfig = "nohook resolv.conf";
-
-  # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   time.timeZone = "America/Los_Angeles";
 
-  # Select internationalisation properties.
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocales = ["ja_JP.UTF-8/UTF-8"];
@@ -54,13 +54,12 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
   # Tablet driver
   hardware.opentabletdriver.enable = true;
 
-  # Enable sound with pipewire.
+  # Audio
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
   services.pipewire = {
@@ -68,15 +67,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.phoenix = {
     isNormalUser = true;
     description = "Lucina Farrell";
@@ -85,7 +77,6 @@
   };
   programs.zsh.enable = true;
 
-  # phoenixmedia nas mount
   fileSystems = {
       "/mnt/phoenixmedia" = {
         device = "10.0.0.21:/";
@@ -98,48 +89,26 @@
   };
   boot.supportedFilesystems = [ "nfs" ];
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    remotePlay.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
   programs.streamdeck-ui = {
     enable = true;
     autoStart = true;
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     home-manager
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  networking.firewall.allowedUDPPorts = [ 5353 31382 ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # DO NOT CHANGE
   system.stateVersion = "25.05"; # Did you read the comment?
 
   # CPU Frequency Governor
@@ -151,7 +120,6 @@
     enable = true;
   };
 
-  # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.graphics.extraPackages = with pkgs; [  intel-vaapi-driver intel-media-driver ];
@@ -183,7 +151,6 @@
 	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
 #    prime = {
