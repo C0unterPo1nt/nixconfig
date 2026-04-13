f69a308a0a84c12824e322d946089d72bdee3d78
C0unterPo1nt <lucinamfarrell@gmail.com>
2026-04-12 19:06:04 -0700

    prettier

--- modules/homeManagerModules/vscode.nix
+++ modules/homeManagerModules/vscode.nix
@@ -1,27 +1,28 @@
-{pkgs, ...}:
-
-{
-    programs.vscode = {
-        enable = true;
-        package = pkgs.vscode.fhs;
-        profiles.phoenix = {
-            extensions = with pkgs.vscode-extensions; [ 
-                bbenoist.nix
-                ms-python.python 
-            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-                {
-                    name = "indenticator";
-                    publisher = "sirtori";
-                    version = "0.7.0";
-                    sha256 = "J5iNO6V5US+GFyNjNNA5u9H2pKPozWKjQWcLAhl+BjY=";
-                }
-            ];
-            userSettings = {
-                "workbench.editor.empty.hint" = "hidden";
-                "workbench.settings.showAISearchToggle" = false;
-                "remote.defaultExtensionsIfInstalledLocally" = [];
-            };
-        };
-        profiles.default.userSettings."window.newWindowProfile" = "phoenix";
+{pkgs, ...}: {
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhs;
+    profiles.phoenix = {
+      extensions = with pkgs.vscode-extensions;
+        [
+          bbenoist.nix
+          ms-python.python
+          esbenp.prettier-vscode
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "indenticator";
+            publisher = "sirtori";
+            version = "0.7.0";
+            sha256 = "J5iNO6V5US+GFyNjNNA5u9H2pKPozWKjQWcLAhl+BjY=";
+          }
+        ];
+      userSettings = {
+        "workbench.editor.empty.hint" = "hidden";
+        "workbench.settings.showAISearchToggle" = false;
+        "remote.defaultExtensionsIfInstalledLocally" = [];
+      };
     };
+    profiles.default.userSettings."window.newWindowProfile" = "phoenix";
+  };
 }
