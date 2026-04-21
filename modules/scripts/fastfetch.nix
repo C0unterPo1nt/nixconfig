39256bc3bcbd56b23614efb0362d84bf99bcbb0b
C0unterPo1nt <lucinamfarrell@gmail.com>
2026-04-20 23:15:48 -0700

    move to new module

--- modules/scripts/fastfetch.nix
+++ modules/scripts/fastfetch.nix
@@ -1,13 +1,13 @@
 {inputs, ...}: {
   perSystem = {pkgs, ...}: {
-    packages.fastfetch = inputs.wrappers.lib.wrapPackage {
+    packages.fastfetch = inputs.wrapper-modules.lib.wrapPackage {
       inherit pkgs;
       package = pkgs.fastfetch;
       flags = {
         "--data" = builtins.readFile ../../assets/anime_wow.txt;
         "-s" = "Title:Separator:OS:Weather:Packages:Shell:DE:WM:Terminal:CPU:GPU:Memory:Break:Colors";
       };
-      preHook = "clear";
+      runShell = ["clear"];
     };
   };
 }
